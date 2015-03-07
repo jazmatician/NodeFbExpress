71aac8b43f07aa732400cf0958416eeed96cb577
jazmatician <jazmatician@gmail.com>
2015-03-07 18:28:13 -0500

    fixed ip for binding

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 var app = require('./app');
 console.log('loaded ../server');
 
+app.set('ipaddress', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
 console.log('port set');
-var server = app.listen(app.get('port'), function () {
+var server = app.listen(app.get('port'), app.get('ipaddress'), function () {
     console.log('Express server listening on port ' + server.address().port);
 });
